fc82e49b1f6b710d3b92d9a8f58fce3540e637bf
TAU2024 <mohamedscitech@outlook.com>
2024-06-20 18:15:35 +0300

    Registeration

--- utils/playwrightUtils.ts
+++ utils/playwrightUtils.ts
@@ -1,5 +1,5 @@
-// Playwright equivalent
-async function clickContinueAs(page: Page) {
+// 
+export async function clickContinueAs(page: Page) {
     const buttonSelector = 'button.custom-btn.w-75.text-center.my-4';
     const button = await page.waitForSelector(buttonSelector);
     if (button) {
@@ -9,26 +9,23 @@ async function clickContinueAs(page: Page) {
     }
 }
 
-// Playwright equivalent
-async function getCountOfCardsinGrid(page: Page, parentElement: string, gridElement: string) {
+// 
+export async function getCountOfCardsinGrid(page: Page, parentElement: string, gridElement: string) {
     await page.waitForSelector(parentElement);
     const gridElements = await page.$$(gridElement);
     const domainAppCountsInit = gridElements.length;
     return domainAppCountsInit;
 }
 
-// Playwright equivalent for findAppCardAndValidate with null handling
-async function findAppCardAndValidate(page: Page, subdomain: string, expectedPrice: any) {
+//  for findAppCardAndValidate with null handling
+export async function findAppCardAndValidate(page: Page, subdomain: string, expectedPrice: any) {
     const gridElements = await page.$$('div.data_body div.grid');
-
     for (const element of gridElements) {
         const subdomainElement = await element.$('div.item.col-2');
         const expectedPriceElement = await element.$('div.item.col-2 + div');
-
         // Optional chaining and nullish coalescing to handle potential null values
         const subdomainText = await subdomainElement?.evaluate(el => el.textContent?.trim() ?? '');
         const expectedPriceText = await expectedPriceElement?.evaluate(el => el.textContent?.trim() ?? '');
-
         // Perform your validations/assertions
         if (subdomainText && expectedPriceText) {
             expect(subdomainText).toContain(subdomain);
@@ -39,7 +36,7 @@ async function findAppCardAndValidate(page: Page, subdomain: string, expectedPri
     }
 }
 
-async function displayingInvalidEmailMessage(page: Page) {
+export async function displayingInvalidEmailMessage(page: Page) {
     const errorMessage = await page.waitForSelector('.errorMessage');
     const textContent = await errorMessage?.innerText();
     if (textContent.trim() !== "Invalid Email Address, the pattern should be :ABC@cba.com") {
@@ -47,13 +44,12 @@ async function displayingInvalidEmailMessage(page: Page) {
     }
 }
 
-async function displayingRequiredMessage(page: Page) {
+export async function displayingRequiredMessage(page: Page) {
     const spanLocator = page.locator('span');
     await expect(spanLocator).toContainText(/required/i);
 }
 
-
-async function increaseScreenItemsCountToHundred(page: Page) {
+export async function increaseScreenItemsCountToHundred(page: Page) {
     const tableRows = await page.$$('table tr');
     const rowCount = tableRows.length;
     if (rowCount > 10) {
@@ -65,7 +61,7 @@ async function increaseScreenItemsCountToHundred(page: Page) {
     }
 }
 
-async function navigateToTheLatestScreen(page: Page) {
+export async function navigateToTheLatestScreen(page: Page) {
     const paginatorCurrent = await page.waitForSelector('.p-paginator-current');
     const pTxt = await paginatorCurrent.innerText();
     let currentPage = pTxt.split("f")[1].toString().trim();
@@ -73,18 +69,20 @@ async function navigateToTheLatestScreen(page: Page) {
         await page.click(`[aria-label="${currentPage}"]`);
     }
 }
-async function scrollToElement(page: Page, containerSelector: string, elementSelector: string) {
+
+export async function scrollToElement(page: Page, containerSelector: string, elementSelector: string) {
     const container = await page.waitForSelector(containerSelector);
     await container.scrollIntoViewIfNeeded();
     const element = await container.waitForSelector(elementSelector);
     await element.scrollIntoViewIfNeeded();
 }
-async function scrollToLastElement(page: Page) {
+
+export async function scrollToLastElement(page: Page) {
     const tableRows = await page.$$('table tbody tr');
     await tableRows[tableRows.length - 1].scrollIntoViewIfNeeded();
 }
 
-async function verifyCorrectColumnsHeaders(page: Page, expectedHeaders: string[]) {
+export async function verifyCorrectColumnsHeaders(page: Page, expectedHeaders: string[]) {
     const tableHeaders = await page.$$('table th');
     for (let i = 0; i < tableHeaders.length; i++) {
         const text = await tableHeaders[i].innerText();
@@ -92,7 +90,7 @@ async function verifyCorrectColumnsHeaders(page: Page, expectedHeaders: string[]
     }
 }
 
-async function getLastCellInTableValue(page: Page, col: number) {
+export async function getLastCellInTableValue(page: Page, col: number) {
     const tableRows = await page.$$('table tbody tr');
     if (tableRows.length > 0) {
         const lastRow = tableRows[tableRows.length - 1];
@@ -107,7 +105,8 @@ async function getLastCellInTableValue(page: Page, col: number) {
         throw new Error('No visible rows in the table.');
     }
 }
-async function verifyALastCellInTable(page: Page, col: number, txt: string) {
+
+export async function verifyALastCellInTable(page: Page, col: number, txt: string) {
     const tableRows = await page.$$('table tbody tr');
     const lastRow = tableRows[tableRows.length - 1];
     const cells = await lastRow.$$('td');
@@ -115,18 +114,19 @@ async function verifyALastCellInTable(page: Page, col: number, txt: string) {
     expect(cellText.trim()).toContain(txt);
 }
 
-async function verifyText(page: Page, index: number, str: string) {
+export async function verifyText(page: Page, index: number, str: string) {
     const input = await page.$$('input[type="text"]');
     const value = await input[index].inputValue();
     expect(value).toEqual(str);
 }
-async function inputText(page: Page, index: number, str: string) {
+
+export async function inputText(page: Page, index: number, str: string) {
     const input = await page.$$('input[type="text"]');
     await input[index].fill(str);
 }
 
 
-async function checkRegExCompatibility(page: Page, fieldSelector: string, inputStr: string, regEx: RegExp) {
+export async function checkRegExCompatibility(page: Page, fieldSelector: string, inputStr: string, regEx: RegExp) {
     await page.fill(fieldSelector, inputStr);
     const val = await page.$eval(fieldSelector, (el) => {
         const inputElement = el as HTMLInputElement;
@@ -136,17 +136,16 @@ async function checkRegExCompatibility(page: Page, fieldSelector: string, inputS
 }
 
 
-async function visibilityOfRequiredStar(page: Page, fieldSelector: string) {
+export async function visibilityOfRequiredStar(page: Page, fieldSelector: string) {
     await page.waitForSelector(fieldSelector, { state: 'attached' });
     const textContent = await page.$eval(fieldSelector, (el) => el.textContent);
-
     if (!textContent || !textContent.includes('*')) {
         throw new Error('Required star (*) is not visible.');
     }
 }
 
 
-async function logOut(page: Page) {
+export async function logOut(page: Page) {
     const profileButton = await page.waitForSelector('button.btn_profaile', { state: 'attached' });
     const isVisible = await profileButton.isVisible();
     if (isVisible) {
@@ -159,21 +158,21 @@ async function logOut(page: Page) {
     }
 }
 
-async function validateTableHeaders(page: Page, headerSelector: string, expectedHeaders: string[]) {
+export async function validateTableHeaders(page: Page, headerSelector: string, expectedHeaders: string[]) {
     const headers = await page.$$eval(headerSelector, els => els.map(el => el.textContent?.trim() || ''));
     for (let i = 0; i < headers.length; i++) {
         expect(headers[i]).toContain(expectedHeaders[i]);
     }
 }
 
-async function validateRequiredComponents(page: Page, len: number) {
+export async function validateRequiredComponents(page: Page, len: number) {
     const elements = await page.$$('span.errorMessage.ng-star-inserted');
     if (elements.length !== len) {
         throw new Error(`Expected ${len} required components, but found ${elements.length}.`);
     }
 }
 
-async function VerifyTableVisibility(page: Page, my_table: string) {
+export async function VerifyTableVisibility(page: Page, my_table: string) {
     await page.waitForSelector(my_table, { state: 'attached' });
     const rows = await page.$$('tr');
     for (const row of rows) {
@@ -192,7 +191,7 @@ async function VerifyTableVisibility(page: Page, my_table: string) {
 }
 
 
-async function setLocalStorage(page: Page, key: string, value: string) {
+export async function setLocalStorage(page: Page, key: string, value: string) {
     await page.evaluate(([key, value]) => {
         localStorage.setItem(key, value);
     }, [key, value]);
@@ -211,11 +210,10 @@ const mail = process.env.mail ?? '';
 const pass = process.env.pass ?? '';
 
 // Define the loginSession function
-async function loginSession(page: Page) {
+export async function loginSession(page: Page) {
     // Clear cookies and local storage before each test
     await page.context().clearCookies();
     await page.goto(loginUrl);
-
     // Fill in the username
     await page.waitForSelector('input[id="Username"]', { state: 'visible', timeout: 15000 });
     await page.fill('input[id="Username"]', mail);
@@ -234,24 +232,22 @@ async function loginSession(page: Page) {
         }
         return false; // Handle other cases if needed
     }, pass);
-
     // Click the login button
     await page.waitForSelector('.custom-btn:has-text("Login")', { state: 'visible' });
     await page.click('.custom-btn:has-text("Login")');
-
     // Verify the URL contains 'bussinessowners'
     await page.waitForURL('**/bussinessowners');
 }
 
 
-async function saveLocalStorageToFile(page: Page, filename: string) {
+export async function saveLocalStorageToFile(page: Page, filename: string) {
     const data = await page.evaluate(() => {
         return JSON.stringify(localStorage);
     });
     // Use a Playwright task or similar approach to write data to a file
     // Example: await page.evaluate(...) and then use Node.js fs module to write to a file
 }
-async function getAllItemsCount(page: Page, gridSelector: string, itemSelector: string) {
+export async function getAllItemsCount(page: Page, gridSelector: string, itemSelector: string) {
     await page.waitForSelector(gridSelector, { state: 'attached' });
     await page.waitForSelector(itemSelector, { state: 'attached' });
     const items = await page.$$(itemSelector);
@@ -271,7 +267,7 @@ type LoginResponse = {
     // Add other properties as needed
 };
 
-async function getLoginResponse(page: Page): Promise<LoginResponse | null> {
+export async function getLoginResponse(page: Page): Promise<LoginResponse | null> {
     const storedLoginResponse = await page.evaluate(() => {
         const storedResponse = window.localStorage.getItem("loginResponse");
         return storedResponse ? JSON.parse(storedResponse) : null;
@@ -282,18 +278,18 @@ async function getLoginResponse(page: Page): Promise<LoginResponse | null> {
 
 
 
-async function checkImageVisibilityBySrc(page: Page, imgSrc: string) {
+export async function checkImageVisibilityBySrc(page: Page, imgSrc: string) {
     await page.waitForSelector(`img[src='${imgSrc}']`, { state: 'visible' });
 }
-export {checkRegExCompatibility}
-export {
-    clickContinueAs, getCountOfCardsinGrid, findAppCardAndValidate, displayingInvalidEmailMessage
-    , displayingRequiredMessage, increaseScreenItemsCountToHundred, navigateToTheLatestScreen,
-    scrollToElement, scrollToLastElement, verifyCorrectColumnsHeaders, getLastCellInTableValue,
-    verifyALastCellInTable, verifyText, inputText, visibilityOfRequiredStar,
-    logOut, validateTableHeaders, validateRequiredComponents, VerifyTableVisibility, setLocalStorage,
-    loginSession, saveLocalStorageToFile, getAllItemsCount, getLoginResponse, checkImageVisibilityBySrc
-};
+// export {checkRegExCompatibility}
+// export {
+//     clickContinueAs, getCountOfCardsinGrid, findAppCardAndValidate, displayingInvalidEmailMessage
+//     , displayingRequiredMessage, increaseScreenItemsCountToHundred, navigateToTheLatestScreen,
+//     scrollToElement, scrollToLastElement, verifyCorrectColumnsHeaders, getLastCellInTableValue,
+//     verifyALastCellInTable, verifyText, inputText, visibilityOfRequiredStar,
+//     logOut, validateTableHeaders, validateRequiredComponents, VerifyTableVisibility, setLocalStorage,
+//     loginSession, saveLocalStorageToFile, getAllItemsCount, getLoginResponse, checkImageVisibilityBySrc
+// };
 
 
 
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { Page } from "playwright";
 
 export async function clearCookies(page: Page) {
     const context = page.context();
